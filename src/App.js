2b7863edd4bc7c7df69a499f78d73ddcea0fae1e
lss <1332456550@qq.com>
2019-08-27 18:02:46 +0800

    init porject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import './style.less'
+import {Row,Col } from 'antd';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import LeftMenu from './components/LeftMenu'
+import './app.less'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Row className="app">
+        <Col className="leftNav" span={3}>
+          <LeftMenu></LeftMenu>
+        </Col>
+        <Col className="right" span={21}>
+          <Header></Header>
+          <div>content</div>
+          <Footer></Footer>
+        </Col>
+      </Row>
+    </Fragment>
   );
 }
 
---
+++ src/components/LeftMenu/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.less'
+
+export default class LeftMenu extends React.Component {
+    render() {
+        return (
+            <div className="left">
+                LeftMenu
+            </div>
+        );
+    }
+    
+}
